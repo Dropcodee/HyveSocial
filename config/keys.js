4fa22113c8a35af8fb3f0f51ae88be791f0e6a0c
Joshua-owolabi <primtech.co@gmail.com>
2018-09-23 23:32:18 +0100

    version 1.0 prepared for deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://DropCode:mykey1@ds155730.mlab.com:55730/dropcodedb",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./Keys_prod");
+} else {
+  module.exports = require("./Keys_dev");
+}
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const users = require("./Routes/api/users");
 const profile = require("./Routes/api/profile");
@@ -34,6 +35,16 @@ mongoose
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+// serve static pages if the app is in production
+if (process.env.NODE_ENV === "production") {
+  // set the static folder to look into
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 const port = process.env.PORT || 3002;
 // End of Routes
 
